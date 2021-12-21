1d7076a0966d7f68463cf52e256848cb296aa9a3
邬晓 <530264127@qq.com>
2021-12-21 23:38:28 +0800

    init

---
+++ config/config.js
@@ -0,0 +1,21 @@
+const config = {
+  mode: 'site',
+  title: '珠峰架构笔记',
+  description: '架构|思路|效率',
+  publicPath: '/',
+  base: '/',
+  exportStatic: {},
+  dynamicImport: {},
+  theme: {
+    '@primary-color': '#00A7D6',
+  },
+  navs: [
+    null
+  ]
+};
+
+if (process.env.NODE_ENV !== 'development') {
+  config.ssr = {};
+}
+
+export default config;
---
+++ docs/node/index.md
@@ -0,0 +1,9 @@
+---
+nav:
+title: Node
+order: 5
+title: Node目录
+order: 1
+---
+
+## Node 目录
